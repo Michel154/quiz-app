af7f5e5e2facfc08b8f57bd12b9b7b9a7089c27f
michel <michel.gorjizadeh@gmail.com>
2026-06-10 00:43:32 +0200

    Lite mer ändringar

--- src/assets/components/Timer.jsx
+++ src/assets/components/Timer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Timer({ setStop, questionNumber }) {
   const [timer, setTimer] = useState(30);
